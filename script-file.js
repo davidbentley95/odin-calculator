15ee87b3b0ff4ce5b4267fffadd9fc1366f21a0c
DBENTLEY <david.bentley95@gmail.com>
2024-11-25 07:02:22 -0800

    Create basic calculator functions.
    Create add, subtract, divide, multiply, percentage and sign change
    functions.

---
+++ script-file.js
@@ -0,0 +1,46 @@
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function percentage(num1) {
+    return num1 / 100;
+}
+
+function changeSign(num1) {
+    return num1 * (-1);
+}
+
+
+// const sum = add(10, 20);
+// console.log(`Add 10+20 = ${sum}`);
+
+// const sub = subtract(25, 5);
+// console.log(`subtract 25-5 = ${sub}`);
+
+// const prod = multiply(4, 3);
+// console.log(`multiply 4*3 = ${prod}`);
+
+// const quotient = divide(25, 5);
+// console.log(`divide 25/5 = ${quotient}`);
+
+// const perc = percentage(78);
+// console.log(`Percentage of 78 = ${perc}`);
+
+// const num = changeSign(14);
+// console.log(`Change sign of 14 = ${num}`);
+
+// const otherNum = changeSign(-6);
+// console.log(`Change sign of -6 = ${otherNum}`);

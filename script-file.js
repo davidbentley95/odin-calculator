4953e070fd867827e4eea19a3ba9f01d3401be8b
DBENTLEY <david.bentley95@gmail.com>
2024-11-28 20:18:03 -0800

    Fix various bugs
    Fix issue associated with negative sign on running total.
    Remove ability to place a operation sign before the first
    number.
    Add click feedback on buttons with CSS.
    Remove double tap zoom on mobile.

--- script-file.js
+++ script-file.js
@@ -9,6 +9,7 @@ let num1 = "";
 let num2 = "";
 let runningTotal = "";
 let updateFirstNum = true;
+let runningTotalCalc = false; 
 let operator = 'addition';
 let screenFormula = document.querySelector(".screen-formula");
 let screenTotal = document.querySelector(".screen-total");
@@ -44,23 +45,27 @@ function changeSign(num1) {
 function setNumberVariables(event) {
     if(updateFirstNum) {
         num1 += event.target.innerText;
-        console.log(Number(num1));
         screenFirstNum.innerText += event.target.innerText;
     } else {
         num2 += event.target.innerText;
-        console.log(Number(num2));
         screenSecondNum.innerText += event.target.innerText;
+        runningTotalCalc = false;
     }
     
 };
 
+
 document.querySelectorAll(".number-button").forEach(item => {item.addEventListener("click", setNumberVariables)});
 
 document.querySelectorAll(".operation-button").forEach(item => {item.addEventListener("click", (event) => {
-    updateFirstNum = false;
-    operator = event.target.id;
-    screenOperator.innerText = event.target.innerText;
-    console.log(operator);
+    if(num1 === "") {
+        
+    } else {
+        updateFirstNum = false;
+        operator = event.target.id;
+        screenOperator.innerText = event.target.innerText;
+    }
+    
 })});
 
 document.querySelector("#clear").addEventListener("click", () => {
@@ -76,12 +81,13 @@ document.querySelector("#clear").addEventListener("click", () => {
 });
 
 document.querySelector(".equals-button").addEventListener("click", () => {
-    runningTotal = OPERATOR_OBJECT[operator](Number(num1), Number(num2))
+    runningTotal = String(OPERATOR_OBJECT[operator](Number(num1), Number(num2)));
     screenTotal.innerText = runningTotal;
     num1 = runningTotal;
     num2 = "";
     screenFirstNum.innerText = runningTotal;
     screenSecondNum.innerText = num2;
+    runningTotalCalc = true;
 });
 
 document.querySelector(".decimal-button").addEventListener("click", () => {
@@ -99,6 +105,12 @@ document.querySelector(".change-sign-button").addEventListener("click", () => {
     if(updateFirstNum && !num1.split("").includes("-")) {
         num1 = "-" + num1;
         screenFirstNum.innerText = num1;
+    }else if(runningTotalCalc && !num1.split("").includes("-")) {
+        num1 = "-" + num1;
+        screenFirstNum.innerText = num1;
+    }else if(runningTotalCalc && num1.split("").includes("-")) {
+        num1 = num1.slice(1);
+        screenFirstNum.innerText = num1;
     }else if(!updateFirstNum && !num2.split("").includes("-")) {
         num2 = "-" + num2;
         screenSecondNum.innerText = num2;
@@ -107,7 +119,7 @@ document.querySelector(".change-sign-button").addEventListener("click", () => {
         screenFirstNum.innerText = num1;
     }else if(!updateFirstNum && num2.split("").includes("-")) {
         num2 = num2.slice(1);
-        screenFirstNum.innerText = num2;
+        screenSecondNum.innerText = num2;
     }
 });
 
